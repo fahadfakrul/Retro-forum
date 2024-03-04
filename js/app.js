2362e8208859b3d5700d3e446c672ed8f005b4ae
Fahad <127460043+fahadfakrul@users.noreply.github.com>
2024-03-04 11:46:59 +0600

    read posts function completed

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const loadPosts = async () => {
+const loadPosts = async (catId) => {
   const response = await fetch(
     "https://openapi.programming-hero.com/api/retro-forum/posts"
   );
@@ -8,11 +8,11 @@ const loadPosts = async () => {
   posts.forEach((post) => {
     const div = document.createElement("div");
     div.classList.add("singlepost");
-    let activeBadge = '';
-    if(post.isActive){
-        activeBadge = `<div class="h-4 w-4 rounded-full bg-[#10B981] absolute -top-1 -right-1"></div>`
-    }else{
-        activeBadge = `<div class="h-4 w-4 rounded-full bg-[#FF3434] absolute -top-1 -right-1"></div>`
+    let activeBadge = "";
+    if (post.isActive) {
+      activeBadge = `<div class="h-4 w-4 rounded-full bg-[#10B981] absolute -top-1 -right-1"></div>`;
+    } else {
+      activeBadge = `<div class="h-4 w-4 rounded-full bg-[#FF3434] absolute -top-1 -right-1"></div>`;
     }
     div.innerHTML = `
     <!-- discussions -->
@@ -44,15 +44,38 @@ const loadPosts = async () => {
                         <p  para-primary">${post.posted_time} min</p>
                     </div>
                     <div class="" >
-                        <img src="./images/email.png" alt="">
+                       <button onclick="readPosts('${post.title}','${post.view_count}')"> <img src="./images/email.png" alt=""></button>
                     </div>
                 </div>
             </div>
 
         </div>
     `;
- discussionContainer.appendChild(div);
+    discussionContainer.appendChild(div);
   });
 };
 
+let count = 0;
+
+const readPosts = (title, view_count) => {
+    const titleReadContainer = document.getElementById("title-read-container");
+    count++;
+    setInnerText("count-read", count);
+  const div = document.createElement("div");
+  div.classList.add("read-post");
+  div.innerHTML = `<div class="w-52 lg:w-64">
+    <p class="text-base font-semibold">${title}</p>
+</div>
+<div class="flex items-center">
+    <img src="./images/eye.png" alt="">
+    <p class="para-primary">${view_count}</p>
+</div>`;
+titleReadContainer.appendChild(div);
+
+  
+};
+function setInnerText(id, value) {
+    document.getElementById(id).innerText = value;
+    console.log(document.getElementById.innerText);
+  }
 loadPosts();
